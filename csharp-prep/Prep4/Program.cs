b55e41f178272918e2c941430f18fe7fdfb084d7
Jess Nelson <jemane.19@gmail.com>
2023-04-27 09:19:32 -0600

    Prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int numb = -1;
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        while (numb != 0)
+        {
+            Console.Write("Enter a number (0 to stop): ");
+            
+            string userResponse = Console.ReadLine();
+            numb = int.Parse(userResponse);
+            
+            // Only add the number to the list if it is not 0
+            if (numb != 0)
+            {
+                numbers.Add(numb);
+            }
+            
+        }
+        
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine(numbers.Count);
+        int max = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                // if this number is greater than the max, we have found the new max!
+                max = number;
+            }
+        }
+
+        Console.WriteLine($"The max is: {max}");
     }
 }
