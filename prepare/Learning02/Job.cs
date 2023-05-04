f7653bc3428d9bf8b25626a72a4e4a7857f9dd77
Jess Nelson <jemane.19@gmail.com>
2023-05-03 22:49:49 -0600

    v2

--- prepare/Learning02/Job.cs
+++ prepare/Learning02/Job.cs
@@ -2,19 +2,19 @@ using System;
  public class Job
     {
         // The C# convention is to start member variables with an underscore _
-        public string _jobTitle = "Meteor";
-        public string _company = "Milky Way Galaxy";
-        public int _startYear = 0;
-        public int _endYear = 10000;
+        public string _jobTitle = "";
+        public string _company = "";
+        public int _startYear;
+        public int _endYear;
 
         // A special method, called a constructor that is invoked using the  
         // new keyword followed by the class name and parentheses.
         
-
         // A method that displays the person's full name as used in eastern 
         // countries or <family name, given name>.
-        public void ShowEasternName()
+        public void ShowJob()
         {
             Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
         }
+        
     }
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -5,5 +5,23 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning02 World!");
+        Job job = new Job();
+        job._jobTitle = "Meteor";
+        job._company = "Milky Way Galaxy";
+        job._startYear = 0;
+        job._endYear = 10000;
+        job.ShowJob();
+        Job job1 = new Job();
+        job1._jobTitle = "Dust";
+        job1._company = "Milky Way Galaxy";
+        job1._startYear = 10000;
+        job1._endYear = 100000;
+        job1.ShowJob();
+        Resume resume = new Resume();
+        resume._name = "Fish";
+        resume._jobs.Add(job);
+        resume._jobs.Add(job1);
+
+        resume.ShowJob();
     }
 }
