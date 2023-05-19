86ce5797dc28e5967f8dec5e4df56d961150b662
Jess Nelson <jemane.19@gmail.com>
2023-05-18 18:02:24 -0600

    Learning3

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -5,5 +5,14 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning03 World!");
+        Fractions f = new Fractions();
+        Console.WriteLine(f.GetFractionString());
+        Console.WriteLine(f.GetDecimalValue());
+        Fractions f1 = new Fractions(3);
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f1.GetDecimalValue());
+        Fractions f2 = new Fractions(5,7);
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f2.GetDecimalValue());
     }
 }
